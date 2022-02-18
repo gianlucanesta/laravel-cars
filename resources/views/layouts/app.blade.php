097000a5801a5f3e5422b4e67167604becd717bc
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-02-18 15:54:30 +0100

    show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="">
     <title>@yield('page_title')</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
-    test
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link" href="{{ route('cars.index')}}">Lista Cars</a>
+                        <a class="nav-link" href="{{ route('cars.create')}}">Crea Nuova Car</a>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        @yield('main_content')
+    </main>
+    
 </body>
 </html>
