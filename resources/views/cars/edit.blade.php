99480b071079146cffb3466ce054587ca91b872a
hexeva <patrick79.g@gmail.com>
2022-02-18 16:57:08 +0100

    created edit page

--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -4,10 +4,52 @@
     EDIT 
 @endsection
 
+    {{-- MESSAGE ERROR --}}
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    {{-- END MESSAGE ERROR --}}
+
 @section('main_content')
 
 <div class="container">
-    IO SONO L'EDIT
+    <form action="{{route('cars.update',['car'=>$car->id])}}" method="post">
+        @csrf
+        @method('PUT')
+
+        <div class="mb-3">
+          <label for="brand" class="form-label">Brand</label>
+          <input type="text" class="form-control" id="brand" name="brand" value="{{old('brand') ? old('brand') : $car->brand}}">
+        </div>
+
+        <div class="mb-3">
+            <label for="model" class="form-label">Model</label>
+            <input type="model" class="form-control" id="model" name="model" value="{{old('model') ? old('model') : $car->model}}">
+          </div>
+
+          <div class="mb-3">
+            <label for="engine_displacement" class="form-label">Engine</label>
+            <input type="text" class="form-control" id="engine_displacement" name="engine_displacement" value="{{old('engine_displacement') ? old('engine_displacement') : $car->engine_displacement}}">
+          </div>
+
+          <div class="mb-3">
+            <label for="doors" class="form-label">Doors</label>
+            <input type="text" class="form-control" id="doors" name="doors" value="{{old('doors') ? old('doors') : $car->doors}}">
+          </div>
+
+          <div class="mb-3">
+            <label for="img" class="form-label">Pictures</label>
+            <input type="text" class="form-control" id="img" name="img" value="{{old('img') ? old('img') : $car->img}}">
+          </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
 </div>
     
 @endsection
