fc70ea2aeb72b15e473b44c86f564ebcd5709c2f
hexeva <patrick79.g@gmail.com>
2022-02-18 13:56:30 +0100

    views scaffold

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>@yield('page_title')</title>
 </head>
 <body>
-    test
+    <header>
+        IO SONO L'header
+    </header>
+    <main>
+        @yield('main_content')
+    </main>
 </body>
 </html>
