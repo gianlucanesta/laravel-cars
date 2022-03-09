fe104269c961e67193ce2227caeb8d61fb51c0a1
Gianluca Nesta <88628541+gianlucanesta@users.noreply.github.com>
2022-03-09 15:14:50 +0100

    car cards

--- resources/views/admin/cars/index.blade.php
+++ resources/views/admin/cars/index.blade.php
@@ -4,27 +4,22 @@
 
     <h1>Lista Cars</h1>
 
-    @foreach ($cars as $car)
-        {{-- <div class="container">
-            <h2>
-                <a href="{{ route('admin.cars.show', ['car' => $car ->id]) }}">{{ $car->brand}}</a>
-            </h2>
-            <div>Tipo: {{ $car->model}}</div>
-        </div> --}}
-
-        <div class="col-2">
-            <div class="card mt-4">
-                <img src="{{ $car ->img}}" class="card-img-top" alt="{{ $car ->img}}" style="width:250px;">
-                <div class="card-body">
-                    <h5 class="card-title font-weight-bold">{{ $car->brand }} {{ $car->model }}</h5>
-                    
-                    <a href="{{ route('admin.cars.show', ['car' => $car ->id]) }}">Entra</a>
+    <div class="container d-flex justify-content-center ">
+        <div class="row row-col-3 ">
+            @foreach ($cars as $car)
+                
+                <div class="card mt-4">
+                    <img src="{{ $car ->img}}" class="card-img-top" alt="{{ $car ->img}}" style="width:250px;">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">{{ $car->brand }} {{ $car->model }}</h5>
+                        
+                        <a href="{{ route('admin.cars.show', ['car' => $car ->id]) }}">Entra</a>
+                    </div>
                 </div>
-            </div>
+
+            @endforeach
         </div>
-
-
-    @endforeach
+    </div>
 
        
 @endsection
